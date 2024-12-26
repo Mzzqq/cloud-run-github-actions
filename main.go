1654c9eaf4aa2f318965c6510c5a66fdf03eaf8f
Mzzqq <rezarifa661@gmail.com>
2024-12-27 01:21:09 +0700

    Add new endpoint

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main() {
 		fmt.Fprintln(w, "Hello, Cloud Run!")
 	})
 
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, it's me Reza!")
+	})
+
 	port := "8080"
 	fmt.Println("Listening on port", port)
 	http.ListenAndServe(":"+port, nil)
